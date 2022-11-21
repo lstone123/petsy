0f8a214ba5959b2f37f0b13b8b9e4403d385a499
Louise Stone <louisemstone@hotmail.com>
2022-11-21 18:07:19 +0000

    fixed log in bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
-  resources :users do
-    resources :sitter_profiles, only: %i[show new create edit update destroy] do
-      resources :bookings, only: %i[edit update]
-    end
-    resources :pets, only: %i[show new create edit update destroy]
-    resources :bookings
-  end
+  devise_for :users
 
+  # resources :users do
+  resources :sitter_profiles, only: %i[show new create edit update destroy] do
+    resources :bookings, only: %i[edit update]
+  end
+  resources :pets, only: %i[show new create edit update destroy]
+  resources :bookings
   resources :sitter_profiles, only: :index
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
