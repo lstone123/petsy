7a783fe96c4fdd7729ac8fc6a138b2b609fdd637
Louise Stone <louisemstone@hotmail.com>
2022-11-22 11:32:48 +0000

    changed validations in user and removed from sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :pets
   has_many :bookings
   has_many :sitter_profiles, through: :bookings
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   # validates :location, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
