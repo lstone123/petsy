52c3a0c12e4bf4497d545b790d12b18ec48b0c7c
Louise Stone <louisemstone@hotmail.com>
2022-11-22 18:05:21 +0000

    created the bookings controller, the index, show, new and create actions and views, need to add edit update and destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,57 @@
 class BookingsController < ApplicationController
+  def index
+    if current_user.sitter == true
+      @user = SitterProfile.where(user_id: current_user)
+      @bookings = Booking.where(sitter_profile_id: @user)
+    else
+      @bookings = current_user.bookings
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @user = current_user
+    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+    @booking = Booking.new(booking_params)
+    @booking.user_id = @user.id
+    @booking.sitter_profile_id = @pet_sitter.id
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = current_user
+    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :owner_message, :user_id, :sitter_profile_id)
+  end
 end
