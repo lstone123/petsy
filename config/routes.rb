d6b1fe862bbf8920f2b1951a8b8e02e4e3229551
Louise Stone <louisemstone@hotmail.com>
2022-11-22 12:40:00 +0000

    amended routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
-
   # resources :users do
-  resources :sitter_profiles, only: %i[show new create edit update destroy] do
-    resources :bookings, only: %i[edit update]
+  resources :sitter_profiles, only: %i[index show] do
+    resources :bookings, only: %i[edit update destroy]
   end
-  resources :pets, only: %i[show new create edit update destroy]
-  resources :bookings
-  resources :sitter_profiles, only: :index
+  resources :pets
+  resources :bookings, only: %i[index show new create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
