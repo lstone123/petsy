0922db7415d87da3be6ecaf5f9d5e37f089ef1e7
Louise Stone <louisemstone@hotmail.com>
2022-11-23 10:53:56 +0000

    updated all views and actions in controllers to be tailored to user types

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,33 +13,47 @@ class BookingsController < ApplicationController
   end
 
   def new
-    @user = current_user
-    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
-    @booking = Booking.new
+    if current_user.sitter == true
+      redirect_to bookings_path
+    else
+      @user = current_user
+      @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+      @booking = Booking.new
+    end
   end
 
   def create
-    @user = current_user
-    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
-    @booking = Booking.new(booking_params)
-    @booking.user_id = @user.id
-    @booking.sitter_profile_id = @pet_sitter.id
-    if @booking.save!
+    if current_user.sitter == true
       redirect_to bookings_path
     else
-      render :new, status: :unprocessable_entity
+      @user = current_user
+      @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+      @booking = Booking.new(booking_params)
+      @booking.user_id = @user.id
+      @booking.sitter_profile_id = @pet_sitter.id
+      if @booking.save!
+        redirect_to bookings_path
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def edit
-    @user = current_user
-    @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
-    @booking = Booking.find(params[:id])
+    if current_user.sitter == true
+      @pet_sitter = SitterProfile.where(user_id: current_user).first
+      @booking = Booking.find(params[:id])
+      @owner = User.find(@booking.user_id)
+    else
+      @pet_sitter = SitterProfile.find(params[:sitter_profile_id])
+      @user = current_user
+      @booking = Booking.find(params[:id])
+    end
   end
 
   def update
     @booking = Booking.find(params[:id])
-    if @booking.update
+    if @booking.update(booking_params)
       redirect_to bookings_path
     else
       render :new, status: :unprocessable_entity
@@ -47,11 +61,14 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :owner_message, :user_id, :sitter_profile_id)
+    params.require(:booking).permit(:start_date, :end_date, :owner_message, :sitter_message, :user_id, :sitter_profile_id, :confirmed)
   end
 end
